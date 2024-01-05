bf973e4ad3dae795b14048b522382452f098a784
Ben-unbelieveable <614347533@qq.com>
2024-01-05 18:03:59 +0800

    20240105

--- Merge_MultImage_2PPT.py
+++ Merge_MultImage_2PPT.py
@@ -1,10 +1,34 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   Merge_MultImage_2PPT.py
+@Time    :   2023/12/12 14:23:22
+@Author  :   Liu.Bo 
+@Version :   1.0.0.0
+@Contact :   liubo4@genomics.cn/614347533@qq.com
+@WebSite :   http://www.ben-air.cn/
+'''
+
+import logging
+from logging.handlers import RotatingFileHandler
+from argparse import ArgumentParser
 import os
+# pip install python-pptx
 from pptx import Presentation
+
+program = 'Merge_MultImage_2PPT.py'
+version = '1.0.0.0'
+
+parser = ArgumentParser(prog=program)
+parser.add_argument('-indir' , dest='input_dir'   , action='store', type=str, help='input  File',default="")
+parser.add_argument('-list', dest='input_dir_list', action='store', type=str, help='output File', default="")
+args = parser.parse_args()
 
 def create_ppt_from_images(directory, output_file):
     # 创建 PowerPoint 对象
     prs = Presentation()
-
+    prs.slide_height = 9144000 #设置slide的高
+    prs.slide_width = 16144000 #设置slide的宽
     # 遍历目录中的图片文件
     for filename in os.listdir(directory):
         if filename.endswith(".jpg") or filename.endswith(".png"):
@@ -19,9 +43,27 @@ def create_ppt_from_images(directory, output_file):
     # 保存 PPT 文件
     prs.save(output_file)
 
-# 指定目录和输出文件名
-image_directory = "test"
-output_ppt = "output.pptx"
+def main():
+    if len(args.input_dir) >1:
+        create_ppt_from_images(args.input_dir, args.input_dir+".pptx")
+    elif len(args.input_dir_list) >0:
+        Dirlist=open(args.input_dir_list,"r", encoding="utf-8")
+        for dir in Dirlist.readlines():
+            dir=dir.strip()
+            create_ppt_from_images(dir,dir+".pptx")
+    else:
+        print("""Usage: 
+python Merge_MultImage_2PPT.py
+    -indir  a Direction contain images 
+    -list   a list of directions contain images
+list Demo>>>
+    /root/dirA
+    /root/dirB
+<<<
 
-# 创建 PPT
-create_ppt_from_images(image_directory, output_ppt)
+              """)
+
+if __name__ == '__main__':
+    main()
+
+
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # AutoWork_toolkits
 一些常用的自动化办公小工具，例如截图整合成PPT、自动生成编辑Word等
+
+## Merge_MultImage_2PPT.py
+用于将多张图片整合到一个ppt文件中，每页一张图片。 支持单文件夹合并，也可以提供多个文件夹的list文件，将分批次处理，每个文件夹生成一个ppt文件。
+```shell
+python Merge_MultImage_2PPT.py
+    -indir  a Direction contain images
+    -list   a list of directions contain images
+```
